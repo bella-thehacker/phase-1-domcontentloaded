bf98d22e5e62e06c7762a4cc7a0ea27672940857
Ivy Nyambura <ikagam018@gmail.com>
2024-07-09 21:33:06 +0300

    solved

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", callBack, (e) =>
+    {
+    console.log("The DOM has Loaded")
+})
+
+function callBack (e){
+    let change = document.getElementById("text")
+    change.innerHTML = "This is really cool!"
+
+}
+console.log(change())
